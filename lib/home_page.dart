b165cc413f08d73f5313d06cfd12c685e124df0f
Lea De Bruijn <leadebruijn1@gmail.com>
2022-05-02 13:24:57 +0200

    api call en camera oopmaak

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,6 +1,8 @@
+import 'dart:io';
 import 'package:api_with_search/search_page.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:image_picker/image_picker.dart';
 import 'post_model.dart';
 import 'dart:convert';
 import 'user.dart';
@@ -14,6 +16,23 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
 
   List<User> _users = [];
+  File? _image;
+
+  Future getImage() async {
+    File image;
+    image = (await ImagePicker().getImage(source: ImageSource.camera)) as File;
+    // if(isCamera){
+    //  image = (await ImagePicker().getImage(source: ImageSource.camera)) as File;
+    //  //image = await _picker.pickImage(source: imageSource.camera);
+    // }
+    // else {
+    //   image = (await ImagePicker().getImage(source: ImageSource.gallery)) as File;
+    //   //image = await _picker.pickImage(source: imageSource.gallery);
+    // }
+    setState(() {
+      _image = image;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,45 +50,58 @@ class _HomePageState extends State<HomePage> {
             )
           ]
       ),
-      body: FutureBuilder(
-        future: _getData(),
-        builder: (ctx, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting)
-            return CircularProgressIndicator();
+      body: Column(
+        children: [
+          FloatingActionButton(
+              child: Icon(Icons.camera_alt),
+              onPressed: () {getImage();}
+          ),
+          FutureBuilder(
+            future: _getData(),
+            builder: (ctx, snapshot) { //ctx = context
+              if (snapshot.connectionState == ConnectionState.waiting)
+                return CircularProgressIndicator();
 
-
-          return ListView.builder(
-            itemBuilder: (ctx, i) {
-              return Column(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  ListTile(
-                    title: Text(_users[i].name),
-                    subtitle: Text(_users[i].number),
-                  ),
-                  Divider(height: 0,)
-                ],
+              return ListView.builder(
+                itemBuilder: (ctx, i) {
+                  return Column(
+                    mainAxisSize: MainAxisSize.max,
+                    children: <Widget>[
+                      ListTile(
+                        title: Text(_users[i].name),
+                        subtitle: Text(_users[i].number),
+                      ),
+                      Divider(height: 0,)
+                    ],
+                  );
+                },
+                itemCount: _users.length,
+                shrinkWrap: true,
               );
             },
-            itemCount: _users.length,
-          );
-        },
+          ),
+        ],
       ),
     );
   }
 
   Future<void> _getData() async {
     // var url = 'https://jsonplaceholder.typicode.com/posts';
-    var url = 'http://192.168.56.1/database.php';
+    // var url = 'http://192.168.56.1/database.php'; //EMULATOR
+    var url = 'http://192.168.1.9:80/database.php'; //MOBILE
+
     http.get(Uri.parse(url)).then((data) {
       return json.decode(data.body);
     }).then((data) {
+      _users.clear();
       for (var json in data) {
-        _users.add(User.fromJson(json));
+        print(json);
+        if(!_users.contains(json)){
+          _users.add(User.fromJson(json));
+        }
       }
-      print(_users);
     })
-        .catchError((e){
+        .catchError((e){ // user kan steeds aangaan al is daar error..
       print(e);
     });
   }
